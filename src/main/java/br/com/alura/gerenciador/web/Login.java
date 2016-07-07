f9b3abaffdb6e24abbb788f5e507fe89eaa9e636
SAMUEL BRISTOT LOLI <samuel.loli@ifsc.edu.br>
2016-07-07 11:00:22 -0300

    alterações

--- src/main/java/br/com/alura/gerenciador/web/Login.java
+++ src/main/java/br/com/alura/gerenciador/web/Login.java
@@ -5,10 +5,10 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.Usuario;
 import br.com.alura.gerenciador.dao.UsuarioDAO;
@@ -31,8 +31,8 @@ public class Login extends HttpServlet{
 			writer.println("<html><body>Usuario Invalido!</body></html>");
 		}
 		else{
-			Cookie cookie = new Cookie("usuario.logado", usuario.getEmail());
-			resp.addCookie(cookie);
+			HttpSession session = req.getSession();
+			session.setAttribute("usuario.logado", usuario);
 			writer.println("<html><body>Usuario logado "+usuario.getEmail()+"</body></html>");
 		}
 	}
---
+++ src/main/java/br/com/alura/gerenciador/web/logout.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class logout extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getSession().removeAttribute("usuario.logado");
+		resp.getWriter().println("<html><body> Deslogado com sucesso </body> </html>");
+	}
+	
+
+}
